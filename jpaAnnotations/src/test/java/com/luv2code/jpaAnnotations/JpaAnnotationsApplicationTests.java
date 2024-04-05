b93b4598bde26b33623bd1e9a3d58ac0cd931558
Dhanasekar-7195 <166021112+Dhanasekar-7195@users.noreply.github.com>
2024-04-05 10:47:22 +0530

    Add files via upload

---
+++ jpaAnnotations/src/test/java/com/luv2code/jpaAnnotations/JpaAnnotationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.jpaAnnotations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAnnotationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
