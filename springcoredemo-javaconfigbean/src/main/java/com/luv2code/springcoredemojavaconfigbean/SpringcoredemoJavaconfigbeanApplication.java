5fe82b3820f032e3add7da0023d3e0de475178b4
Dhanasekar-7195 <166021112+Dhanasekar-7195@users.noreply.github.com>
2024-04-05 09:33:55 +0530

    Add files via upload

---
+++ springcoredemo-javaconfigbean/src/main/java/com/luv2code/springcoredemojavaconfigbean/SpringcoredemoJavaconfigbeanApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springcoredemojavaconfigbean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcoredemoJavaconfigbeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoJavaconfigbeanApplication.class, args);
+	}
+
+}
---
+++ springcoredemo-javaconfigbean/src/main/java/com/luv2code/springcoredemojavaconfigbean/common/Coach.java
@@ -0,0 +1,6 @@
+package com.luv2code.springcoredemojavaconfigbean.common;
+
+public interface Coach {
+	String getDailyWorkout();
+
+}
