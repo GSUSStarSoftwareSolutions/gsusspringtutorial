f20e8fcf5a3fd191200a391c2e170250cd569a36
Dhanasekar-7195 <166021112+Dhanasekar-7195@users.noreply.github.com>
2024-04-06 16:13:47 +0530

    Add files via upload

---
+++ springBootRestPathVariables/src/main/java/com/luv2code/springBootRestPathVariables/SpringBootRestPathVariablesApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springBootRestPathVariables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestPathVariablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestPathVariablesApplication.class, args);
+	}
+
+}
---
+++ springBootRestPathVariables/src/test/java/com/luv2code/springBootRestPathVariables/SpringBootRestPathVariablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.springBootRestPathVariables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestPathVariablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
