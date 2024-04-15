73dd4eed6aa9e8fe0e40c79a3ea511a78a4d91ed
Dhanasekar-7195 <166021112+Dhanasekar-7195@users.noreply.github.com>
2024-04-15 14:17:47 +0530

    Add files via upload

---
+++ cruddemoMappings-1/src/test/java/com/luv2code/cruddemoMappings/CruddemoMappings1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luv2code.cruddemoMappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoMappings1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
